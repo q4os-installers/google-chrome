878a1033ffb5d61c130f23c45d49accc75f67fc5
Q4OS Team <q4os@q4os.org>
2025-02-14 17:36:52 +0100

    Replace kdialog with a dedicated wrapper for more general usage in different desktop environments

--- filesystem/opt/program_files/q4os-chrome/bin/check_gchr_version.sh
+++ filesystem/opt/program_files/q4os-chrome/bin/check_gchr_version.sh
@@ -15,7 +15,7 @@ CURR_VER="$( $CHROME_REAL_EXEC --version )"
 if [ "$LAST_VER" != "$CURR_VER" ] ; then
   kwriteconfig --file "$CHROME_CFGDIR/goochrq4rc" --group "Misc" --key "LastVersion" "$CURR_VER"
   if [ "$LAST_VER" != "" ] ; then
-    kdialog --title "" --caption "" --icon "google-chrome" --passivepopup "<font size=4><p>$LAST_VER has been updated since last run.</p><p>New version: <b>$CURR_VER</b></p></font>" 20 &
+    /usr/lib/kdialogwr/kdialog --title "" --caption "" --icon "google-chrome" --passivepopup "<font size=4><p>$LAST_VER has been updated since last run.</p><p>New version: <b>$CURR_VER</b></p></font>" 20 &
   fi
 fi
 rm -f $STAMPFL1
